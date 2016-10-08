7ba30ccc85fcb03f5f88ecf302a99d18b0f69c3c
Peter Bøg <phb@combine.dk>
2016-10-08 21:26:20 +0300

    Added Pusher in frontend

--- public/js/board.js
+++ public/js/board.js
@@ -1,22 +1,59 @@
+/* global pusher */
+/* exported board */
+
 var board = {
+	pusherChannelTest: null,
+
 	init: function() {
+		this.initBoardCells();
+		this.initPlayerTiles();
+		this.initPusherChannel();
+	},
+
+	/**
+	 * Initialize the board cells and tiles.
+	 */
+	initBoardTiles: function() {
 		$('.board-cell.droppable').droppable({
 			drop: function(event, ui) {
 				var toX = parseInt($(this).data('x'));
 				var toY = parseInt($(this).data('y'));
 				var tileId = ui.draggable.data('id');
 				// @todo If move failed we might need to move it back?
-				board.moveTile(tileId, toX, toY);
-			}
-		});
-		$('#player-tiles').droppable({
-			drop: function(event, ui) {
-				var tileId = ui.draggable.data('id');
-				board.removeTile(tileId);
+				board.movePlayerTile(tileId, toX, toY);
 			}
 		});
 		$('.tile.draggable').draggable({
 			
+		});
+	},
+
+	/**
+	 * Initialize the player tiles.
+	 */
+	initPlayerTiles: function() {
+		$('#player-tiles').droppable({
+			drop: function(event, ui) {
+				var tileId = ui.draggable.data('id');
+				board.removePlayerTile(tileId);
+			}
+		});
+	},
+
+	/**
+	 * Initialize the Pusher channels.
+	 */
+	initPusherChannel: function() {
+		pusher.init();
+		this.pusherChannel = pusher.pusher.subscribe('board');
+		this.pusherChannel.bind('tileMoved', function(data) {
+			board.moveTile(data.tileId, data.toX, data.toY);
+		});
+		this.pusherChannel.bind('tileRemoved', function(data) {
+			board.removeTile(data.tileId);
+		});
+		this.pusherChannel.bind('gameOver', function(data) {
+			board.showGameOver();
 		});
 	},
 
@@ -27,16 +64,57 @@ var board = {
 	 * @param {Number} toX
 	 * @param {Number} toY
 	 */
-	moveTile: function(tile, toX, toY) {
+	movePlayerTile: function(tile, toX, toY) {
 		// @todo Make server request!
 	},
 
 	/**
-	 * Remove a tile from the board (back to the players tiles)
+	 * Remove a player tile from the board (back to the players tiles)
 	 *
 	 * @param {String} tile
 	 */
-	removeTile: function(tile) {
+	removePlayerTile: function(tile) {
 		
+	},
+
+	/**
+	 * Move a tile on the board.
+	 *
+	 * @param {String} tileId
+	 * @param {Number} toX
+	 * @param {Number} toY
+	 */
+	moveTile: function(tileId, toX, toY, data) {
+		var $tile = $('.tile[data-id="' + tileId + '"]').detach();
+		if ($tile.length === 0) {
+			// Tile does not exists, must be another player adding one - create it
+			$tile = $('<div class="tile tile-' + data.type + ' draggable" data-id="' + tileId + '"></div>');
+		}
+		$('.board-cell[data-x="' + toX + '"][data-y="' + toY + '"]').append($tile);
+	},
+
+	/**
+	 * Remove a tile from the board (except player tiles).
+	 *
+	 * @param {String} tileId
+	 */
+	removeTile: function(tileId) {
+		var $tile = $('.tile[data-id="' + tileId + '"]');
+		if ($tile.data('is_player_tile')) {
+			return; // Do not remove the own players tiles (if he have hacked his own tiles, who cares!)
+		}
+		$tile.remove();
+	},
+
+	/**
+	 * Show the level complete!
+	 */
+	showGameOver: function() {
+		$('#gameover_success_modal').modal({
+			backdrop: 'static',
+			keyboard: false,
+			show: true
+		});
 	}
+
 };
