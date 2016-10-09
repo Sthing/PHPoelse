f9a528d364c318d54963fce7df6d6024701ff468
Peter Bøg <phb@combine.dk>
2016-10-09 14:14:34 +0300

    Minor frontend adjustments.

--- public/js/pusher.js
+++ public/js/pusher.js
@@ -2,7 +2,26 @@
 /* exported pusher */
 
 var pusher = {
+	/**
+	 * An instance of the Pusher API library.
+	 *
+	 * @type Pusher
+	 */
 	pusher: null,
+
+	/**
+	 * The Pusher App key.
+	 *
+	 * @type String
+	 */
+	appKey: '88de40d153eadda15eb5',
+
+	/**
+	 * The Pusher App cluster.
+	 *
+	 * @type String
+	 */
+	appCluster: 'eu',
 
 	/**
 	 * Initialize Pushser
@@ -18,8 +37,8 @@ var pusher = {
 	 * Stores it in the pusher variable.
 	 */
 	startPusher: function() {
-		this.pusher = new Pusher('88de40d153eadda15eb5', {
-			cluster: 'eu',
+		this.pusher = new Pusher(this.appKey, {
+			cluster: this.appCluser,
 			encrypted: true
 		});
 	}
